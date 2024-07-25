766403143714388be6b86c5b257c488ade07aa5f
Felipe <fhora93@gmail.com>
2024-07-25 11:38:15 -0300

    Todas funções adicionadas

--- func.py
+++ func.py
@@ -1,21 +1,17 @@
 import requests
 import pandas as pd
+import os
+
+def clear_screen():
+    # Limpa a tela do terminal
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 def get_data(url, headers):
     response = requests.get(url, headers=headers)
-    response.raise_for_status()  # Raise an exception if the request was unsuccessful
+    response.raise_for_status() 
     data = response.json()['data']
     df = pd.DataFrame(data)
     return df
-
-url = "https://ultimate-tennis1.p.rapidapi.com/global_players_stats/serve/2024/all"
-headers = {
-    "X-RapidAPI-Key": "743d67516fmsh1c6f10058aec683p1ae6a3jsna3a372a01620",
-    "X-RapidAPI-Host": "ultimate-tennis1.p.rapidapi.com"
-}
-df = get_data(url, headers)
-print(df.info())
-#PlayerName, AvgAcesPerMatchSortField, AvgDblFaultsPerMatchSortField, FirstServePctSortField, ServiceGamesWonPctSortField
-
 
 def aces_avg():
     url = "https://ultimate-tennis1.p.rapidapi.com/global_players_stats/serve/2024/all"
@@ -29,6 +25,71 @@ def aces_avg():
     aces = df_aces.sort_values(by='AvgAcesPerMatchSortField', ascending=False).iloc[:15].reset_index(drop=True)
     aces.index = aces.index + 1
     return aces
+
+def aces():
+    url = "https://ultimate-tennis1.p.rapidapi.com/global_players_stats/serve/2024/all"
+    headers = {
+        "X-RapidAPI-Key": "743d67516fmsh1c6f10058aec683p1ae6a3jsna3a372a01620",
+        "X-RapidAPI-Host": "ultimate-tennis1.p.rapidapi.com"
+    }
+
+    df = get_data(url, headers)
+    df_acesp = df[['PlayerName', 'AvgAcesPerMatch']]
+    acesp = df_acesp.sort_values(by='AvgAcesPerMatch', ascending=False).iloc[:15].reset_index(drop=True)
+    acesp.index = acesp.index + 1
+    return acesp
+
+def first_serve():
+    url = "https://ultimate-tennis1.p.rapidapi.com/global_players_stats/serve/2024/all"
+    headers = {
+        "X-RapidAPI-Key": "743d67516fmsh1c6f10058aec683p1ae6a3jsna3a372a01620",
+        "X-RapidAPI-Host": "ultimate-tennis1.p.rapidapi.com"
+    }
+
+    df = get_data(url, headers)
+    df_first_serve = df[['PlayerName', 'FirstServePctSortField']]
+    first_serve = df_first_serve.sort_values(by='FirstServePctSortField', ascending=False).iloc[:15].reset_index(drop=True)
+    first_serve.index = first_serve.index + 1
+    return first_serve
+
+def first_serve_pct():
+    url = "https://ultimate-tennis1.p.rapidapi.com/global_players_stats/serve/2024/all"
+    headers = {
+        "X-RapidAPI-Key": "743d67516fmsh1c6f10058aec683p1ae6a3jsna3a372a01620",
+        "X-RapidAPI-Host": "ultimate-tennis1.p.rapidapi.com"
+    }
+
+    df = get_data(url, headers)
+    df_first_serve_pct = df[['PlayerName', 'FirstServePointsWonPctSortField']]
+    first_serve_pct = df_first_serve_pct.sort_values(by='FirstServePointsWonPctSortField', ascending=False).iloc[:15].reset_index(drop=True)
+    first_serve_pct.index = first_serve_pct.index + 1
+    return first_serve_pct
+
+def double_fault():
+    url = "https://ultimate-tennis1.p.rapidapi.com/global_players_stats/serve/2024/all"
+    headers = {
+        "X-RapidAPI-Key": "743d67516fmsh1c6f10058aec683p1ae6a3jsna3a372a01620",
+        "X-RapidAPI-Host": "ultimate-tennis1.p.rapidapi.com"
+    }
+
+    df = get_data(url, headers)
+    df_double_fault = df[['PlayerName', 'AvgDblFaultsPerMatchSortField']]
+    double_fault = df_double_fault.sort_values(by='AvgDblFaultsPerMatchSortField', ascending=False).iloc[:15].reset_index(drop=True)
+    double_fault.index = double_fault.index + 1
+    return double_fault
+
+def second_service():
+    url = "https://ultimate-tennis1.p.rapidapi.com/global_players_stats/serve/2024/all"
+    headers = {
+        "X-RapidAPI-Key": "743d67516fmsh1c6f10058aec683p1ae6a3jsna3a372a01620",
+        "X-RapidAPI-Host": "ultimate-tennis1.p.rapidapi.com"
+    }
+
+    df = get_data(url, headers)
+    df_second_service = df[['PlayerName', 'SecondServePointsWonPctSortField']]
+    second_service = df_second_service.sort_values(by='SecondServePointsWonPctSortField', ascending=False).iloc[:15].reset_index(drop=True)
+    second_service.index = second_service.index + 1
+    return second_service    
 
 def servicewon():
     url = "https://ultimate-tennis1.p.rapidapi.com/global_players_stats/serve/2024/all"
--- main.py
+++ main.py
@@ -1,4 +1,49 @@
-import func
+from func import *
+# print("--------------------  Menu de Opções  --------------------")
+# print("1. Aces média")
+# print("2. Média de aces por partida")
+# print("3. First Serve")
+# print("4. Porcentagem de Primeiro serviço")
+# print("5. Média Dupla Falta por jogo")
+# print("6. Média de Segundo Serviço Ganhos")
+# print("7. Porcentagem Serviços Ganhos")
+# print("8. Sair")
+# print()
+while True:
+    clear_screen()
 
-
-print (aces_avg())
+    print("--------------------  Menu de Opções  --------------------")
+    print("1. Aces média")
+    print("2. Média de aces por partida")
+    print("3. First Serve")
+    print("4. Porcentagem de Primeiro serviço")
+    print("5. Média Dupla Falta por jogo")
+    print("6. Média de Segundo Serviço Ganhos")
+    print("7. Porcentagem Serviços Ganhos")
+    print("8. Sair")
+    print()
+    choice = int(input("Selecione um Número "))
+    clear_screen()
+    if choice == 1:
+        aces_avg()
+    elif choice == 2:
+        aces()
+    elif choice == 3:
+        first_serve()
+    elif choice == 4:
+        first_serve_pct()
+    elif choice == 5:
+        double_fault()
+    elif choice == 6:
+        second_service()    
+    elif choice == 7:
+        servicewon()
+    elif choice == 8:
+        print("Saindo...")
+        break
+    else:
+        print("Opção inválida. Por favor, selecione uma opção válida.")
+    # except ValueError:
+    #     print("Erro: Por favor, insira um número válido.")
+    
+    # input("\nPressione Enter para voltar ao menu principal...")
